a57d69cbec99296068f947ae16576a93a926e4a6
ben stelzer <benstelzer@ben-stelzers-MacBook-Pro.local>
2016-05-25 10:26:56 -0700

    initial commit

---
+++ ngApp/shared/app.ts
@@ -0,0 +1,13 @@
+namespace app {
+  angular.module("app", ["ui.router"])
+  .config((
+    $urlRouterProvider: ng.ui.IUrlRouterProvider,
+    $locationProvider: ng.ILocationProvider
+
+  ) => {
+
+    $urlRouterProvider.otherwise("/");
+    $locationProvider.html5Mode(true);
+  });
+
+}
